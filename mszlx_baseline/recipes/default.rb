400dea3c69980959bfb7c482945eb8cdaac887e8
Mario Szpuszta <marioszp@microsoft.com>
2020-01-09 19:42:43 +0000

    Got iptables working

--- mszlx_baseline/recipes/default.rb
+++ mszlx_baseline/recipes/default.rb
@@ -36,20 +36,49 @@ dpkg_package 'webmin.dpkg' do
 end
 
 #
+# Setup iptables by running the default cookbook recipe for iptables
+#
+include_recipe 'iptables::default'
+
+#
 # iptables Firewall rules for the external network adapter
 #
-iptables_chain 'fw_external' do
-    chain 'FW_EXTERNAL'
+iptables_rule 'outbound_allowall' do
+    table 'filter'
+    chain 'OUTPUT'
+    match '-o eth0 -d 0.0.0.0/0'
+    target 'ACCEPT'
 end
 
-iptables_rule 'ssh_external' do
-    chain 'FW_EXTERNAL'
+iptables_rule 'outbound_allowall_response' do
+    table 'filter'
+    chain 'INPUT'
+    match '-i eth0 -m state --state ESTABLISHED,RELATED'
+    target 'ACCEPT'
+end
+
+iptables_rule 'inbound_ssh_external' do
+    table 'filter'
+    chain 'INPUT'
     match '-p tcp --dport 22 -i eth0'
     target 'ACCEPT'
 end
 
-iptables_rule 'deny_all' do
-    chain 'FW_EXTERNAL'
+iptables_rule 'inbound_webmin_external' do
+    table 'filter'
+    chain 'INPUT'
+    match '-p tcp --dport 10000 -i eth0'
+    target 'ACCEPT'
+end
+
+iptables_rule 'inbound_deny_all' do
+    table 'filter'
+    chain 'INPUT'
     match '-i eth0'
-    target 'DENY'
+    target 'DROP'
+end
+
+service 'netfilter-persistent' do
+    action :restart
+    retries 3
 end
