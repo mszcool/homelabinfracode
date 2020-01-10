1e9055cac5e07b2a44a8db35bf8c6b954254572d
Mario Szpuszta <marioszp@microsoft.com>
2020-01-10 20:54:58 +0100

    refactor: switching back to cockpit from webmin

--- mszlx_baseline/recipes/baseline.rb
+++ mszlx_baseline/recipes/baseline.rb
@@ -12,27 +12,37 @@
 # 
 # Baseline package requirements (also required for Webmin)
 #
-apt_package 'perl'
-apt_package 'perl-openssl-abi-1.1'
-apt_package 'libnet-ssleay-perl'
-apt_package 'openssl'
-apt_package 'libauthen-pam-perl'
-apt_package 'libpam-runtime'
-apt_package 'libio-pty-perl'
-apt_package 'apt-show-versions'
-apt_package 'python'
+package 'perl'
+package 'perl-openssl-abi-1.1'
+package 'libnet-ssleay-perl'
+package 'openssl'
+package 'libauthen-pam-perl'
+package 'libpam-runtime'
+package 'libio-pty-perl'
+package 'apt-show-versions'
+package 'python3'
+
+#
+# Cockpit: for basic web-based administration tasks
+#
+package 'cockpit'
+
+service 'cockpit.socket' do
+    action [ :enable, :start ]
+    retries 3
+end
 
 #
 # Webmin used for Web-based adhoc administration if needed
 #
-remote_file '/home/marioszp/downloads/webmin.dpkg' do
-    source 'http://prdownloads.sourceforge.net/webadmin/webmin_1.940_all.deb'
-    owner 'marioszp'
-    mode '0600'
-    action :create 
-end
+# remote_file '/home/marioszp/downloads/webmin.dpkg' do
+#     source 'http://prdownloads.sourceforge.net/webadmin/webmin_1.940_all.deb'
+#     owner 'marioszp'
+#     mode '0600'
+#     action :create 
+# end
 
-dpkg_package 'webmin.dpkg' do
-    source '/home/marioszp/downloads/webmin.dpkg'
-    action :install
-end
+# dpkg_package 'webmin.dpkg' do
+#     source '/home/marioszp/downloads/webmin.dpkg'
+#     action :install
+# end
