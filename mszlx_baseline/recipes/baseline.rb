0e7efebdf1b2867e9b5aed58da3d159863da6a3f
Mario Szpuszta <marioszp@microsoft.com>
2020-01-10 09:20:29 +0000

    Splitting into multiple recipes

---
+++ mszlx_baseline/recipes/baseline.rb
@@ -0,0 +1,27 @@
+# 
+# Baseline package requirements (also required for Webmin)
+#
+apt_package 'perl'
+apt_package 'perl-openssl-abi-1.1'
+apt_package 'libnet-ssleay-perl'
+apt_package 'openssl'
+apt_package 'libauthen-pam-perl'
+apt_package 'libpam-runtime'
+apt_package 'libio-pty-perl'
+apt_package 'apt-show-versions'
+apt_package 'python'
+
+#
+# Webmin used for Web-based adhoc administration if needed
+#
+remote_file '/home/marioszp/downloads/webmin.dpkg' do
+    source 'http://prdownloads.sourceforge.net/webadmin/webmin_1.940_all.deb'
+    owner 'marioszp'
+    mode '0600'
+    action :create 
+end
+
+dpkg_package 'webmin.dpkg' do
+    source '/home/marioszp/downloads/webmin.dpkg'
+    action :install
+end
