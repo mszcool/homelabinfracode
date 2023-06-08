2921c2665e566794316f44aae4ffa7bd8471ec07
Mario Szpuszta <marioszp@microsoft.com>
2023-06-09 00:27:15 +0200

    Fixes and updates

---
+++ scripts/create-garden-edge.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# To remember, how I created the gardenedge host for the home environment.
+
+if [ "$SKIP_BOOTSTRAP" = "true" ]; then
+    ansible-playbook --inventory ./configs.private/hosts.ini \
+                    --limit "gardenedge" \
+                    --extra-vars "@configs.private/pi-edge-config-extra-vars.yaml" \
+                    --extra-vars "skip_bootstrap=true" \
+                    ./playbooks/create-edge-multiwifi.yaml 
+else
+    ansible-playbook --inventory ./configs.private/hosts.ini \
+                    --limit "gardenedge" \
+                    --extra-vars "@configs.private/pi-edge-config-extra-vars.yaml" \
+                    ./playbooks/create-edge-multiwifi.yaml 
+fi
+wait
