22bfb27d70869bea0b1ad13899ec7635002d9757
Mario Szpuszta-Strobl <marioszp@microsoft.com>
2024-04-06 22:45:44 +0200

    Fix in setup script for pooledge

--- scripts/create-pool-edge-bridge.sh
+++ scripts/create-pool-edge-bridge.sh
@@ -4,13 +4,13 @@
 
 if [ "$SKIP_BOOTSTRAP" = "true" ]; then
     ansible-playbook --inventory ./configs.private/infra-bootstrap/hosts.ini \
-                    --limit "gardenedge" \
+                    --limit "pooledge" \
                     --extra-vars "@configs.private/infra-bootstrap/pi-edge-bridge-pool-extra-vars.yaml" \
                     --extra-vars "skip_bootstrap=true" \
                     ./playbooks/create-edge-bridgewifi.yaml
 else
     ansible-playbook --inventory ./configs.private/infra-bootstrap/hosts.ini \
-                    --limit "gardenedge" \
+                    --limit "pooledge" \
                     --extra-vars "@configs.private/infra-bootstrap/pi-edge-bridge-pool-extra-vars.yaml" \
                     ./playbooks/create-edge-bridgewifi.yaml
 fi
