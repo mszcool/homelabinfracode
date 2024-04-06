b04baa6108d2e532b27aae8f3283d0d99f4f32fc
Mario Szpuszta-Strobl <marioszp@microsoft.com>
2024-04-06 22:39:03 +0200

    Updating with new infra for pool maintenance

--- scripts/create-pool-edge-bridge.sh
+++ scripts/create-pool-edge-bridge.sh
@@ -5,13 +5,13 @@
 if [ "$SKIP_BOOTSTRAP" = "true" ]; then
     ansible-playbook --inventory ./configs.private/infra-bootstrap/hosts.ini \
                     --limit "gardenedge" \
-                    --extra-vars "@configs.private/infra-bootstrap/pi-edge-bridge-garden-extra-vars.yaml" \
+                    --extra-vars "@configs.private/infra-bootstrap/pi-edge-bridge-pool-extra-vars.yaml" \
                     --extra-vars "skip_bootstrap=true" \
                     ./playbooks/create-edge-bridgewifi.yaml
 else
     ansible-playbook --inventory ./configs.private/infra-bootstrap/hosts.ini \
                     --limit "gardenedge" \
-                    --extra-vars "@configs.private/infra-bootstrap/pi-edge-bridge-garden-extra-vars.yaml" \
+                    --extra-vars "@configs.private/infra-bootstrap/pi-edge-bridge-pool-extra-vars.yaml" \
                     ./playbooks/create-edge-bridgewifi.yaml
 fi
 wait
